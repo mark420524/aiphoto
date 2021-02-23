b1f899c9b6113ff674e229988ae6cd79213b61d2
mark <markjava@foxmail.com>
2021-02-23 16:31:20 +0800

    增加格式化

--- m_web/upload.py
+++ m_web/upload.py
@@ -28,51 +28,51 @@ class UploadHandler(tornado.web.RequestHandler):
         if not width or  not height or  not color:
             self.write('error parameter')
         else:
-        for inputname in filesDict:
-            # 第一层循环取出最外层信息，即input标签传回的name值
-            # 用过filename键值对对应，取出对应的上传文件的真实属性
-            http_file = filesDict[inputname]
-            for fileObj in http_file:
+            for inputname in filesDict:
+                # 第一层循环取出最外层信息，即input标签传回的name值
+                # 用过filename键值对对应，取出对应的上传文件的真实属性
+                http_file = filesDict[inputname]
+                for fileObj in http_file:
 
-                # 第二层循环取出完整的对象
-                # 取得当前路径下的 upfiles 文件夹+上fileObj.filename属性(即真实文件名)
-                filePath = os.path.join(parent_path, "static", filename+".jpg")
-                
-                with open(filePath, 'wb') as f:
-                    f.write(fileObj.body)
-
-
-
-        org_img = filePath
-
-        id_image = os.path.join(parent_path, "static", filename+"id.png")
-        # 20200719
-        # 通过识别人脸关键点，裁剪图像
-        ai_crop.crop_photo(org_img,id_image, width, height )
+                    # 第二层循环取出完整的对象
+                    # 取得当前路径下的 upfiles 文件夹+上fileObj.filename属性(即真实文件名)
+                    filePath = os.path.join(parent_path, "static", filename+".jpg")
+                    
+                    with open(filePath, 'wb') as f:
+                        f.write(fileObj.body)
 
 
-        
-        alpha_img = os.path.join(parent_path, "static", filename+"_alpha.png")
-        
-        alpha_resize_img = os.path.join(parent_path, "static", filename+"_alpha_resize.png")
-        
-        #
-        # 通过u_2_net 获取 alpha
-        my_u2net_test.test_seg_trimap(id_image, alpha_img, alpha_resize_img)
-        #
-        # # 通过alpha 获取 trimap
-        trimap = os.path.join(parent_path, "static", filename+"_trimap_resize.png")
-        to_standard_trimap.to_standard_trimap(alpha_resize_img, trimap)
-        
 
-        id_image_org = os.path.join(parent_path, "static", filename+"id_2in.png")
+            org_img = filePath
 
-        #
-        # 证件照添加蓝底纯色背景//"..\\aiphoto\\img\\meinv_trimap_resize.png"
-        #         to_standard_trimap.to_standard_trimap(alpha_resize_img, trimap)
-        to_background.to_background(id_image, trimap, id_image_org, color)
-        
-        self.write( "static/"+filename+"_meinv_id_2in.png")
+            id_image = os.path.join(parent_path, "static", filename+"id.png")
+            # 20200719
+            # 通过识别人脸关键点，裁剪图像
+            ai_crop.crop_photo(org_img,id_image, width, height )
+
+
+            
+            alpha_img = os.path.join(parent_path, "static", filename+"_alpha.png")
+            
+            alpha_resize_img = os.path.join(parent_path, "static", filename+"_alpha_resize.png")
+            
+            #
+            # 通过u_2_net 获取 alpha
+            my_u2net_test.test_seg_trimap(id_image, alpha_img, alpha_resize_img)
+            #
+            # # 通过alpha 获取 trimap
+            trimap = os.path.join(parent_path, "static", filename+"_trimap_resize.png")
+            to_standard_trimap.to_standard_trimap(alpha_resize_img, trimap)
+            
+
+            id_image_org = os.path.join(parent_path, "static", filename+"id_2in.png")
+
+            #
+            # 证件照添加蓝底纯色背景//"..\\aiphoto\\img\\meinv_trimap_resize.png"
+            #         to_standard_trimap.to_standard_trimap(alpha_resize_img, trimap)
+            to_background.to_background(id_image, trimap, id_image_org, color)
+            
+            self.write( "static/"+filename+"_meinv_id_2in.png")
 
 
 
